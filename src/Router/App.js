e3372fd97cc0c3deb42c9264c9b8d6f6bb86f477
Fangjie <s3577269@student.rmit.edu.au>
2023-03-24 12:34:00 +1100

    second

--- src/Router/App.js
+++ src/Router/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react'
-import { Route,Routes } from "react-router-dom";
+import { Route,Routes,Link } from "react-router-dom";
 import Login from './pages/login';
 import Create from './pages/create';
 import IndexPage from './pages/indexpage';
-
+import User from './pages/user';
 
 export default class App extends Component {
   render() {
     return (
       <div>
+        <ul>
+          <li><Link to="/login">login</Link></li>
+          <li><Link to="/atlas">sign up</Link></li>
+          <li><Link to="/atlas/GT">user</Link></li>
+        </ul>
       <Routes>
         <Route path="/" element={<IndexPage />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/create" element={<Create />} />
+        <Route path="/atlas" element={<Create />} />
+        <Route path="/atlas/:id" element={<User />}/>
       </Routes>
       </div>
     )
